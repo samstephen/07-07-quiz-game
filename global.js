0a27fe1468362442b3437ec022ed457a8aba7ca4
Sam Stephen <samueljstephen@gmail.com>
2015-07-07 16:20:33 -0500

    finished question objects

--- global.js
+++ global.js
@@ -1,3 +1,34 @@
 /**
  * Created by Samuel Stephen & Patrick Carlton on 7/7/15.
  */
+
+
+// The user is presented an alert() with a multiple choice question
+
+var count = 0;
+var score = 0;
+
+
+
+
+
+var question1 = {   question: "What are you?",
+                    possibleAnswers: "(a) human, (b) robot, (c) unicorn, (d) chicken",
+                    rightAnswer: "a"};
+var question2 = {   question: "Where is OCS?",
+                    possibleAnswers: "(a) Omaha, (b) Elmwood, (c) Nehawka, (d) Lincoln",
+                    rightAnswer: "a"};
+var question3 = {   question: "What's the right answer?",
+                    possibleAnswers: "(a) this one, (b) not this one, (c) not this one, (d) not this one",
+                    rightAnswer: "a"};
+var question4 = {   question: "Pick something.",
+                    possibleAnswers: "(a) Stop picking a, (b) I'm bored, (c) yup, (d) Chicken-Wang",
+                    rightAnswer: "a"};
+
+
+// answer alerts
+document.write("Of course you are a human. DUH!")
+document.write("Omaha - tagline goes here.")
+document.write("You're a modern, Sherlock!")
+document.write("Good choice!")
+
